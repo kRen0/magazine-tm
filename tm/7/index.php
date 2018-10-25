3b318dd80663fa9ad4bd77e71f6cc41cf8154ea4
kRen0 <hubizza@gmail.com>
2018-10-26 01:15:35 +0300

    27-th

---
+++ tm/7/index.php
@@ -0,0 +1,12 @@
+<?php
+if(!isset($_GET['uri'])) {
+    $url = 'https://s3.eu-west-3.amazonaws.com/magazine-tm/7/index.html';
+}
+else {
+    $url = 'https://s3.eu-west-3.amazonaws.com/magazine-tm/7/' . $_GET['uri'];
+}
+@$content = file_get_contents($url);
+foreach($http_response_header as $header)
+    header($header, FALSE);
+die($content);
+?>
